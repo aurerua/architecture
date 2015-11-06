392d373c6f0a796ecb902e2aaf566578c96d5ec0
aurerua <aure@posteo.de>
2015-11-06 13:30:32 +0000

    Completed the implementation of the 'kontakt' page.

---
+++ site/templates/studio.php
@@ -0,0 +1,44 @@
+<?php snippet('header') ?>
+
+<?php snippet('navigation') ?>
+
+ <div class="site-wrapper">
+
+    <div class="container post-header">
+        <div class="row gutter-60">
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-8">
+                <header>
+                  <?php foreach($page->images() as $image): ?>
+                    <a href="<?php echo $image->url() ?>">
+                      <img class="img-responsive" src="<?php echo $image->url() ?>" alt="">
+                    </a>
+                  <?php endforeach ?>
+                </header>
+            </div>
+        </div>
+    </div>
+    
+
+    <main class="content" role="main">
+        <div class="container post people">
+            <div class="row gutter-60">
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-8">
+                    <article>
+                        <header class="post-header">
+                        </header>
+                        <section class="post-content">
+                            <?php echo $page->text()->kirbytext() ?>
+                        </section>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    
+    
+<?php snippet('footer') ?> Hi
