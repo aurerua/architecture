7d018940be32c52ad71c5615eba47e9ee31c7432
aurerua <aure@posteo.de>
2015-11-06 15:02:33 +0000

    Added a field in the admin to the form for the 'Kontakt' page, where a Google map can be given in the form of an iframe.

---
+++ site/blueprints/kontakt.php
@@ -0,0 +1,15 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Page
+pages: false
+files: false
+fields:
+  title:
+    label: Title
+    type:  text
+  text:
+    label: Text
+    type:  textarea
+  karte:
+    label: Karte
+    type: text
