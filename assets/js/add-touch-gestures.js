cdcff7b707b70227998f73ad6bb6d6be1738649f
aurerua <aure@posteo.de>
2015-11-18 13:49:00 +0000

    1.Touch gestures script is now checking if viewing device is a touch device. 2.Fixed a few design issues on smaller screens.

--- assets/js/add-touch-gestures.js
+++ assets/js/add-touch-gestures.js
@@ -8,15 +8,20 @@ $(document).ready(function() {
   var theCarousel = $('#carousel-example-generic');
   // Create a simple 'hammer' instance
   var hammer = new Hammer(theCarousel[0]); // The element is passed to hammer, not the jquery object.
-  // Move to the next slide if the carousel is tapped, or moved to the left
-  hammer.on('panleft swipeleft', function(ev) {
-    console.log('to the left');
-    theCarousel.carousel('next');
-  });
-  // Move to the previous slide if the carousel is moved to the right
-  hammer.on('panright swiperight', function(ev) {
-    console.log('to the right');
-    theCarousel.carousel('prev');
-  });
+  // Only add touch gestures for touch devices
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+  // Move to the next slide if the carousel is tapped, or swiped to the left
+    hammer.on('panleft swipeleft', function(ev) {
+        theCarousel.carousel('next');
+      });
+    // Move to the previous slide if the carousel is swiped to the right
+    hammer.on('panright swiperight', function(ev) {
+      theCarousel.carousel('prev');
+    });
+  } else {
+    hammer.on('tap', function(ev) {
+      theCarousel.carousel('next');
+    });
+  };
 });
  
