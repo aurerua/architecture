f014b53b7d8b669e846f564b40840627c8d5830a
aurerua <aure@posteo.de>
2015-11-30 17:41:08 +0000

    Fixed URL for google fonts.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -18,7 +18,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 
-  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato" />
 
   <!-- Bootstrap -->
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
